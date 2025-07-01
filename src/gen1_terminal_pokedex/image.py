ec376c4cd35598d130f2d74243a0b137149e41cd
PericarpoQ <jonathagh@gmail.com>
2025-07-01 09:10:24 -0300

    fix: remove error handling

--- src/gen1_terminal_pokedex/image.py
+++ src/gen1_terminal_pokedex/image.py
@@ -14,7 +14,7 @@ import io
 
 import ascii_magic
 import requests
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
 
 
 def process_image(image_url: str, columns: int = 64) -> str:
@@ -31,23 +31,18 @@ def process_image(image_url: str, columns: int = 64) -> str:
         str: A string containing the generated ASCII art.
 
     """
-    try:
-        response = requests.get(image_url, timeout=5)
-        response.raise_for_status()  # Raise an exception for bad status codes
-        image_data = io.BytesIO(response.content)
-        with Image.open(image_data) as img:
-            # To avoid overwriting the `with` statement variable, use new variables
-            img_for_processing = img.convert("RGBA") if img.mode != "RGBA" else img
+    response = requests.get(image_url, timeout=5)
+    response.raise_for_status()  # Raise an exception for bad status codes
+    image_data = io.BytesIO(response.content)
+    with Image.open(image_data) as img:
+        # To avoid overwriting the `with` statement variable, use new variables
+        img_for_processing = img.convert("RGBA") if img.mode != "RGBA" else img
 
-            # Get the bounding box of the non-transparent parts
-            bbox = img_for_processing.getbbox()
+        # Get the bounding box of the non-transparent parts
+        bbox = img_for_processing.getbbox()
 
-            # Crop the image if a bounding box is found
-            final_img = img_for_processing.crop(bbox) if bbox else img_for_processing
+        # Crop the image if a bounding box is found
+        final_img = img_for_processing.crop(bbox) if bbox else img_for_processing
 
-            # Generate ASCII art from the cropped image
-            return ascii_magic.from_pillow_image(final_img).to_ascii(columns=columns)
-    except requests.exceptions.RequestException as e:
-        return f"Error fetching image: {e}"
-    except UnidentifiedImageError:
-        return "Error: The provided file is not a valid image."
+        # Generate ASCII art from the cropped image
+        return ascii_magic.from_pillow_image(final_img).to_ascii(columns=columns)
