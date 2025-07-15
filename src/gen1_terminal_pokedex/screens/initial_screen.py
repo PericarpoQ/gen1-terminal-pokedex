4b1f5e7aa5282e73e8ab1f68d8c681e4a05da21f
PericarpoQ <jonathagh@gmail.com>
2025-07-15 08:47:33 -0300

    refactor: add logo to initial screen

--- src/gen1_terminal_pokedex/screens/initial_screen.py
+++ src/gen1_terminal_pokedex/screens/initial_screen.py
@@ -1,33 +1,34 @@
 """Module to create the pokemon initial screen."""
 
-from src.gen1_terminal_pokedex.pokemon import Pokemon
+from src.gen1_terminal_pokedex.screens.utils import LOGO
 
 
-def get_initial_screen(pokemon: Pokemon) -> str:
+def get_initial_screen() -> str:
     """Generate string that represents the pokemon initial menu as a screen.
-
-    Args:
-        pokemon (Pokemon): Pokemon object whose screen will be created.
 
     Returns:
         str: initial menu to be printed as a screen.
 
     """
-    p = pokemon
     total_width = 132
-    title = f"{p.id} - {p.name.upper()}"
-    centered_title = title.center(total_width)
-
-    menu_options = ["1 - Information", "2 - Learnset", "3 - Areas Found"]
-
     screen = []
+
+    logo_lines = LOGO.copy()
+
     screen.append(f"╔{'═' * total_width}╗")
-    screen.append(f"║{centered_title}║")
+
+    for line in logo_lines:
+        padding = (total_width - 70) // 2
+        screen.append(
+            f"║{' ' * padding}{line.center(total_width - padding)}{' ' * padding}║"
+        )
+
     screen.append(f"╠{'═' * total_width}╣")
 
-    for option in menu_options:
-        line = option.center(total_width)
-        screen.append(f"║{line}║")
-
+    prompt = "Welcome to the Terminal Pokédex. Insert a Pokémon name or number.".center(
+        total_width
+    )
+    screen.append(f"║{prompt}║")
     screen.append(f"╚{'═' * total_width}╝")
+
     return "\n".join(screen)
